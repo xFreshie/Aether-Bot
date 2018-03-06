5f8dfb76774ae12cfa674224b4899c20f3ef4365
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-03-06 21:54:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,5 +34,6 @@ bot.on('guildMemberAdd', member => {
 bot.on('guildMemberRemove', member => {
     member.guild.channels.get('404273665925185536').send('**' + member.user.username + '**, has left the server');
     //
+    }	
 });
 bot.login(process.env.BOT_TOKEN);
