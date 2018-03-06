ad4523b1a3d61948d02fa94317fc8c55a1157603
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-03-06 22:00:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const Discord = require("discord.js");
-const client = new Discord.Client);
-const PREFIX = ".";
+const client = new Discord.Client({
+  disableEveryone: true,
+  messageCacheMaxSize: 500,
+  messageCacheLifetime: 120,
+  messageSweepInterval: 60
+});
+const Cleverbot = require("cleverbot-node");
+const economy = require('discord-eco');
+const fs = require('fs');
+const YTDL = require('ytdl-core');
+const sql = require("sqlite");
+const clbot = new Cleverbot;
+clbot.configure({botapi: "CC74amM7cQZbj-XPp-OAY1pesPw"});
+ 
+const PREFIX = "-";
 
-//Commands
     if (!message.content.startsWith(PREFIX)) return;
    
     var args = message.content.substring(PREFIX.length).split(" ");
@@ -16,13 +28,14 @@ const PREFIX = ".";
             .setColor(("#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); })))
             message.channel.send(embed);
 break;
-//Welcome GoodBye
-bot.on('guildMemberAdd', member => {
-    member.guild.channels.get('404273665925185536').send('**' + member.user.username + '**, has joined the server!'); 
+            //Welcome And GoodBye
+            bot.on('guildMemberAdd', member => {
+                member.guild.channels.get('404273665925185536').send('**' + member.user.username + '**, has joined the server!'); 
+            });
 
-bot.on('guildMemberRemove', member => {
-    member.guild.channels.get('404273665925185536').send('**' + member.user.username + '**, has left the server');
-    //
+            bot.on('guildMemberRemove', member => {
+                member.guild.channels.get('404273665925185536').send('**' + member.user.username + '**, has left the server');
+                //
     }	
-};
+});
 bot.login(process.env.BOT_TOKEN);
